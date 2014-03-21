d9d193bb58f08dbe51bf845aad9e19b1ca6ee37c
Adrian Teaca <adrian.teaca.12@aberdeen.ac.uk>
2014-03-21 18:43:02 +0000

    mobile_friendly_maybe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
 	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
+  def mobile_device?
+	request.user_agent =~ /Mobile|webOS/
+  end
+  helper_method :mobile_device?
+  
 end
